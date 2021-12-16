44951bbe151b23989e0813b7de007c4471920ef8
redassser <redryan890@gmail.com>
2021-12-16 00:50:21 -0500

    fine days work

--- public/screen.js
+++ public/screen.js
@@ -1,98 +1,79 @@
 import React, {useEffect, useRef, useState} from "react";
+const SimplexNoise = require("simplex-noise");
 
 function Screen(props) {
-    const [thex,setx] = useState(0);
-    const [they,sety] = useState(0);
-
-    const resolution = 30; var length = 0;
-    const xamo = 800/resolution; const yamo = 800/resolution;
+    const [resc, resChange] = useState(50);
+    const [seedc, seedChange] = useState(Math.random());
+    const [choo, chooChange] = useState(0);
+    const [sens, sensChange] = useState(10);
+    const [inpc, inpChange] = useState([resc, seedc, choo, sens]);
 
     const canvasRef = useRef(null);
 
-    function randoCol() {
-        const posCol = ["red", "green", "blue","cyan", "yellow", "white","black","grey","purple","orange"];
-        return posCol[Math.floor(Math.random()*posCol.length)];
+    //returns random color
+    function randoCol(x,y,seed,noisetype, sensitivity) {
+            switch(noisetype) {
+                case 1:
+                    
+                    break;
+                default:
+                    const simplex = new SimplexNoise(seed)
+                    return "rgba(255,255,255,"+simplex.noise2D(x/sensitivity,y/sensitivity)+")";
+                    break;
+            }
     }
-    function randoShp() {
-        const posShp = [["s","r","d","r"],["s","d","r","d"],["s","l","d","u"],["s","d","d","d"]];
-        return posShp[Math.floor(Math.random()*posShp.length)];
-    }
-    function get(x,y) {
-        if (x<1||y<1) return "Please use an integer greater than 0";
-        if (x>resolution||y>resolution) return "Please use an integer within resolution";
-        return grid[resolution-(y)][x-1];
-    }
-    let grid = [];
-    function createGrid(context) {
-        grid = [];
+    //THIS is createGrid, otherwise known as how this thing works. Makes the grid you can reference
+    function createGrid(context, resolution) {
+        let grid = {
+            units:[],
+            fill(x,y,color) {
+                const xamo = 800/resolution; const yamo = 800/resolution;
+                context.fillStyle = color;
+                grid.units[y][x]=color;
+                context.fillRect(x*xamo,y*yamo,xamo,yamo);
+            }
+        };
         for(let y=0;y<resolution;y++) {
             var row = []; 
             for(let x=0;x<resolution+1;x++) {
-                row[x] = null
+                row[x] = null;
             }
-            grid[y]=row;
+            grid.units[y]=row;
         }
         return grid;
     }
-    function draw(context) {
-        createGrid(context);
+    //mosaic grid
+    function noise(context, resolution, seed, noisetype, sensitivity) {
+        var blockGrid = createGrid(context, resolution);
         for(let y=0;y<resolution;y++) {
             for(let x=0;x<resolution+1;x++) {
-                const color = (grid[y][x]===null) ? randoCol() : "black";
-                context.fillStyle = color;
-                var xh=x; var yh=y; var shpar = []
-                var ar = randoShp()
-                for (const step of ar) {
-                    switch (step) {
-                        case "s":
-                            break;
-                        case "u":
-                            yh=(yh-1<resolution)?yh-1:yh;
-                            break;
-                        case "d":
-                            yh=(yh+1<resolution)?yh+1:yh;
-                            break;
-                        case "l":
-                            xh=(xh-1<resolution)?xh-1:xh;
-                            break;
-                        case "r":
-                            xh=(xh+1<resolution)?xh+1:xh;
-                            break;
-                        default:
-                            break;
-                    }
-                    if(grid[yh][xh]!=null) {
-                        break;
-                    } else {
-                        grid[yh][xh]=color;
-                        context.fillRect(xh*xamo,yh*yamo,xamo,yamo);
-                    }
-                }
+                const color = (blockGrid.units[y][x]===null) ? randoCol(x,y,seed,noisetype, sensitivity) : "black";
+                blockGrid.fill(x,y,color);
             }
         }
     }
-
+    //basically what the Screen actually returns
     useEffect(()=> {
         const canvas = canvasRef.current;
         const context = canvas.getContext("2d");
         canvas.height = "800";
         canvas.width = "800";
-        draw(context);
-    }, [draw])
+        noise(context, inpc[0], inpc[1], inpc[2], inpc[3]);
+    }, [inpc])
     return (
         <>
+            <div id="header">
+                <button className="inp" onClick={()=>{const v=Math.random();inpChange([resc, v, choo, sens]);seedChange(v);}}>Random Seed</button>
+                <button className="inp" onClick={()=>inpChange([resc, seedc, choo, sens])}>Update Grid</button>
+                <input className="inp" type="number" value={seedc} onChange={(x)=>{seedChange(x.target.value)}}/>
+                <input className="inp" type="number" value={resc} onChange={(x)=>{resChange(x.target.value)}}/>
+                <input className="inp" type="range" min="1" max="30" value={sens} onChange={(x)=>{sensChange(x.target.value)}}/>
+                <input className="inp" name="choose" type="radio" id="noise" onChange={(x)=>{if(x.target.value==="on")chooChange(0);}} defaultChecked/> <label className="inp" htmlFor="noise">Simplex</label>
+                <input className="inp" name="choose" type="radio" id="perlin" onChange={(x)=>{if(x.target.value==="on")chooChange(1);}}/> <label className="inp" htmlFor="perlin">Perlin</label>
+            </div>
             <canvas ref={canvasRef} {...props}></canvas>
         </>
     )
 }
-
-/*
-<div id="head">
-                X<input id="x" className="inp" onChange={n=>{setx(n)}}></input>
-                Y<input id="y" className="inp" onChange={n=>{sety(n)}}></input>
-                <button className="inp" onClick={()=>{console.log(get(thex,they))}}/>
-                <p id="colo" className="inp">h</p>
-            </div>
-*/
 
 export default Screen
