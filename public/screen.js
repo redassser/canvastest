9b5c972101ca1f9456da7bfef2c20964d9de3685
redassser <redryan890@gmail.com>
2021-12-17 16:17:18 -0500

    final

--- public/screen.js
+++ public/screen.js
@@ -2,11 +2,11 @@ import React, {useEffect, useRef, useState} from "react";
 import SimplexNoise from 'simplex-noise';
 
 function Screen(props) {
-    const [resc, resChange] = useState(100);
+    const [resc, resChange] = useState(200);
     const [seedc, seedChange] = useState(Math.random());
     const [choo, chooChange] = useState(1);
     const [sens, sensChange] = useState(.6);
-    const [zoom, zoomChange] = useState(10);
+    const [zoom, zoomChange] = useState(5);
     const [soup, refresh] = useState(0);
 
     const canvasRef = useRef(null);
@@ -16,16 +16,28 @@ function Screen(props) {
         const xamo = canvas.width/resc; const yamo = canvas.height/resc;
         for(let x=0;x<resc;x++) {
             for(let y=0;y<resc;y++) {
-                var r = simplex.noise2D(x/zoom, y/zoom)*sens
+                var r = simplex.noise2D(x/(zoom*5), y/(zoom*5))*sens*.6
                 switch(Math.floor(r*10)) {
-                    case (0):
-                        r="0,0,255";
+                    case (5):
+                        r="255,255,255";
                         break;
-                    case (1):
+                    case (4):
+                        r="155,255,195";
+                        break;
+                    case (3):
+                        r="100,255,100";
+                        break;
+                    case (2):
                         r="0,255,0";
                         break;
+                    case (1):
+                        r="75,255,0";
+                        break;
+                    case (0):
+                        r="255,255,175";
+                        break;
                     default:
-                        r="255,0,0"
+                        r="0,0,255"
                         break;
                 }
                 context.fillStyle = "rgba("+r+",255)";
@@ -39,7 +51,7 @@ function Screen(props) {
         var data = imgData.data; var res = canvas.height;
         for(var x=0;x<res;x++) {
             for(var y=0;y<res;y++) {
-                var r = simplex.noise2D(x/zoom,y/zoom)*sens;
+                var r = simplex.noise2D(x/(zoom*5), y/(zoom*5))*sens;
                 data[(x + y * res) * 4 + 0] = 255;
                 data[(x + y * res) * 4 + 1] = 255;
                 data[(x + y * res) * 4 + 2] = 255;
@@ -69,14 +81,14 @@ function Screen(props) {
             <div>
                 <button className="inp" onClick={()=>{seedChange(Math.random);refresh(soup+1)}}>Random Seed</button>
                 <button className="inp" onClick={()=>refresh(soup+1)}>Update Grid</button>
-                <input className="inp" type="number" value={seedc} onChange={(x)=>{seedChange(x.target.value)}}/>
-                <input className="inp" type="number" value={resc} onChange={(x)=>{resChange(x.target.value)}}/>
+                <input className="inp" type="range" min="1" max="30" step="1" id="zoom" value={zoom} onChange={(x)=>{zoomChange(x.target.value)}}/> <label className="inp" htmlFor="zoom">Zoom</label>
+                <input className="inp" type="range" min="0.1" max=".9" step=".1" id="sens" value={sens} onChange={(x)=>{sensChange(x.target.value)}}/> <label className="inp" htmlFor="sens">sens</label>
             </div>
             <div>
-            <input className="inp" type="range" min="1" max="30" step="1" id="zoom" value={zoom} onChange={(x)=>{zoomChange(x.target.value)}}/> <label className="inp" htmlFor="zoom">Zoom</label>
-                <input className="inp" type="range" min="0.1" max=".9" step=".1" id="sens" value={sens} onChange={(x)=>{sensChange(x.target.value)}}/> <label className="inp" htmlFor="sens">sens</label>
-                <input className="inp" name="choose" type="radio" id="noise" onChange={(x)=>{if(x.target.value==="on")chooChange(0);}}/> <label className="inp" htmlFor="noise">Simplex full</label>
-                <input className="inp" name="choose" type="radio" id="perlin" onChange={(x)=>{if(x.target.value==="on")chooChange(1);}} defaultChecked/> <label className="inp" htmlFor="perlin">Image Gen</label>
+            <input className="inp" type="number" value={seedc} onChange={(x)=>{seedChange(x.target.value)}}/>
+                <input className="inp" type="number" value={resc} onChange={(x)=>{resChange(x.target.value)}}/>
+                <input className="inp" name="choose" type="radio" id="noise" onChange={(x)=>{if(x.target.value==="on")chooChange(0);}}/> <label className="inp" htmlFor="noise">Simplex Archipelago</label>
+                <input className="inp" name="choose" type="radio" id="perlin" onChange={(x)=>{if(x.target.value==="on")chooChange(1);}} defaultChecked/> <label className="inp" htmlFor="perlin">Image Greyscale</label>
             </div>
             <canvas ref={canvasRef} {...props}></canvas>
         </>
